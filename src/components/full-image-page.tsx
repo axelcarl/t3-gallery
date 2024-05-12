3ed143bd5c0ce143e561d5b695adc928ae553d58
Axel Carlsson <axelthescarecrow@gmail.com>
2024-05-12 15:17:11 +0200

    Modal/Dialog changes

--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -1,7 +1,18 @@
 import { getImage } from "~/server/queries";
 
 export default async function FullPageImageView(props: { id: number }) {
-  const image = await getImage(props.id);
+    const image = await getImage(props.id);
 
-  return <img src={image.url} className="w-96" />;
+    return (
+        <div className="flex h-full w-full min-w-0">
+
+            <div className="flex-shrink flex items-center align-items">
+                <img src={image.url} className="object-contain flex-shrink" />
+            </div>
+
+            <div className="flex w-48 flex-shrink-0 flex-col">
+                <div className="text-xl font-bold">{image.name}</div>
+            </div>
+        </div>
+    )
 }
